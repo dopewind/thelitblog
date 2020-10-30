bab8edfcfdcdd6990cf10c72d954802652cb1e9c
dopewind <imkvinayan@gmail.com>
2020-10-30 15:35:19 +0530

    modified:   .travis/push.sh
    Hopefully fixed https://app.codacy.com/gh/dopewind/dopewind.github.io/pullRequest?prid=6428596

--- .travis/push.sh
+++ .travis/push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/thelitblog/thelitblog.github.io.git > /dev/null 2>&1
+  "git remote add origin-pages https://${GH_TOKEN}@github.com/thelitblog/thelitblog.github.io.git > /dev/null 2>&1"
   git push --quiet --set-upstream origin gh-pages 
 }
 
