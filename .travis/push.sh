176a912d46118e8afb5c807a7b9bd60b402792ec
dopewind <imkvinayan@gmail.com>
2020-10-31 17:53:16 +0530

    New Approach to push
    	modified:   .travis.yml
    	deleted:    .travis/id.pem.enc
    	modified:   .travis/push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,42 +1,40 @@
 #!/bin/sh
+# Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
 
-git clone https://github.com/thelitblog/thelitblog.github.io.git
-cp -r _site/* ./thelitblog.github.io/
+commit_country_json_files() {
+  git checkout master
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git reset
+  git add -f ./_site/
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
 
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://dopewind:${GITHUB_TOKEN}@github.com/thelitblog/ > /dev/null 2>&1
+  git push origin master --quiet
+}
 
-openssl aes-256-cbc -K $encrypted_502dca1ab0b2_key -iv $encrypted_502dca1ab0b2_iv -in identity.pem.enc -out identity.pem -d
-eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 .id_rsa.pem # this key should have push access
-ssh-add identity.pem
+setup_git
 
-cd thelitblog.github.io
-git remote add deploy git@github.com:thelitblog/thelitblog.github.io.git
-git rm -r --cached ./
-git checkout main
-git add -f _site/
-git push deploy main
+commit_country_json_files
 
-
-
-# comment this out later
-# setup_git() {
-#   git config --global user.email "travis@travis-ci.org"
-#   git config --global user.name "Travis CI"
-# }
-
-# commit_website_files() {
-#   git checkout main
-#   git add ./_site/ -f
-#   git commit -m "Travis build: $TRAVIS_BUILD_NUMBER" -a
-# }
-
-# upload_files() {
-#   git remote rm origin
-#   "git remote add origin https://${GITHUB_TOKEN_LIT}@github.com/thelitblog/thelitblog.github.io.git > /dev/null 2>&1"
-#   git push --quiet --set-upstream origin main 
-# }
-
-# setup_git
-# commit_website_files
-# upload_files
+# Attempt to commit to git only if "git commit" succeeded
+if [ $? -eq 0 ]; then
+  echo "Uploading to GitHub"
+  upload_files
+else
+  echo "Nothing to do"
+fi
